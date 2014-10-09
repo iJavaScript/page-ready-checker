366825d0b597aa7d596409291a87d34157d3d7db
Haisheng.Wu.mbp <freizl@gmail.com>
2014-10-09 11:40:00 +0800

    All use specify pageReadyEls; be able to delay certain after AJAX stop in order to check whther there is no more ajax.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,25 +4,30 @@
    // Init page ready checker
    // ================================================================================
 
-   PR.init({
-      debug: true,
-      timeout: 15000,
+    try {
+        PR.init({
+            debug: true,
+            timeout: 15000,
 
-      criticalEls: ['#div2-internal', '#div3-internal', '#jsonp-internal'],
-      //criticalElsFn: function () { return false; },
+            criticalEls: ['#div2-internal', '#div3-internal', '#jsonp-internal'],
+            //criticalElsFn: function () { return false; },
 
-      criticalFn: function () {
-         console.warn(" ================ CRITICAL ELEMENTS DONE ===========");
-      },
+            criticalFn: function () {
+                console.warn(" ================ CRITICAL ELEMENTS DONE ===========");
+            },
 
-      pageReadyFn: function () {
-         console.warn(" ================ PAGE DONE ===========");
-      },
+            pageReadyFn: function () {
+                console.warn(" ================ PAGE DONE ===========");
+            },
 
-      timeoutFn: function () {
-         console.warn(" ================ PAGE Timout ===========");
-      }
-   });
+            timeoutFn: function () {
+                console.warn(" ================ PAGE Timout ===========");
+            }
+        });
+
+    } catch(error) {
+        console.error(error);
+    }
 
    $.ajax({url: 'http://publicfeed.stubhub.com/listingCatalog/select/?version=2.2&q=urlpath:(dallas-cowboys-tickets%20dallas-cowboys-arlington-at-t-stadium-10-5-2014-9037680)%20AND%20active:1%20AND%20allowedViewingDomain:[*%20TO%20*]&fq=&start=0&rows=10&fl=id%20description%20timezone%20event_date_time_local%20channelId%20artistPrimaryStyle%20ancestorGenreIds%20ancestorGeoIds%20ancestorDescriptions%20ancestorGenreDescriptions%20city%20state%20venue_name%20urlpath%20genreUrlPath%20venue_config_id%20geography_parent%20is_scrubbing_enabled%20view_from_section_ind%20book_of_business_id%20image_url%20channel%20bundled_type%20venue_config_name%20lat_lon%20minPrice%20currency_code%20hide_event_date%20hide_event_time%20venue_configuration_version%20venue_config_map_type%20totalPostings%20genre_parent_name%20pageType%20venue_config_ga_ind%20totalTickets%20name_primary_en_US%20seo_description_en_US%20keywords_en_US%20seo_title_en_US%20id%20description%20leaf%20pageType%20urlpath%20sportsTeam%20channelId%20artistPrimaryStyle%20ancestorGenreIds%20ancestorGeoIds%20ancestorDescriptions%20ancestorGenreDescriptions%20gameType%20genre_parent_name%20canHaveEventsFlag%20genre_description_en_US%20seo_description_en_US%20keywords_en_US%20seo_title_en_US%20%20stubhubDocumentType&wt=json&json.wrf=?',
 
@@ -84,7 +89,6 @@
                    $('body').append('<p style="clear:both; color:red;">' + JSON.stringify(res)  + '</p>');
                }
            });
-
 
    }
 
